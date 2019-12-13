1629539bc1b14d2aaa142ec924a6a01ca8963ef8
Leonardo Fontana <leo.d.fontana@gmail.com>
2019-12-13 20:40:14 -0300

    Corrigido o Projeto. Inteiro.

--- WondermazingGameStore.UserAccount.Web/Controllers/AccountController.cs
+++ WondermazingGameStore.UserAccount.Web/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using WondermazingGameStore.UserAccount.Web.Models;
 
+
 namespace WondermazingGameStore.UserAccount.Web.Controllers
 {
     [Authorize]
